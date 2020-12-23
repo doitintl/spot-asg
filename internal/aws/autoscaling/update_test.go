9b58cbe620d164605546554518644fd9df4ffec4
Alexei Ledenev <alexei.led@gmail.com>
2020-12-23 16:12:53 +0200

    lint with golangci-lint implement update ASG 1st draft (similar types and weights by vCPU) logic to search for similar instance type
    Signed-off-by: Alexei Ledenev <alexei.led@gmail.com>

---
+++ internal/aws/autoscaling/update_test.go
@@ -0,0 +1,90 @@
+package autoscaling
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_getGoodCandidates(t *testing.T) {
+	type args struct {
+		instanceType string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []instanceTypeWeight
+	}{
+		{
+			"get candidates for m5.4xlarge: general purpose 16vCPU",
+			args{"m5.4xlarge"},
+			[]instanceTypeWeight{
+				{"m5.4xlarge", 16},
+				{"m4.4xlarge", 16},
+				{"m5n.4xlarge", 16},
+				{"m5ad.4xlarge", 16},
+				{"m5dn.4xlarge", 16},
+				{"m5d.4xlarge", 16},
+				{"m5a.4xlarge", 16},
+				{"m4.2xlarge", 8},
+				{"m5n.2xlarge", 8},
+				{"m5ad.2xlarge", 8},
+				{"m5d.2xlarge", 8},
+				{"m3.2xlarge", 8},
+				{"m5.2xlarge", 8},
+				{"m5dn.2xlarge", 8},
+				{"m5zn.2xlarge", 8},
+				{"m5a.2xlarge", 8},
+				{"m5zn.3xlarge", 12},
+				{"m5zn.6xlarge", 24},
+				{"m5dn.8xlarge", 32},
+				{"m5d.8xlarge", 32},
+				{"m5a.8xlarge", 32},
+				{"m5ad.8xlarge", 32},
+				{"m5.8xlarge", 32},
+				{"m5n.8xlarge", 32},
+			},
+		},
+		{
+			"get candidates for t3.large: burstable 2 vPCU",
+			args{"t3.large"},
+			[]instanceTypeWeight{
+				{"t3.large", 2},
+				{"t3.medium", 2},
+				{"t2.large", 2},
+				{"t3a.large", 2},
+				{"t3a.medium", 2},
+				{"t3.nano", 2},
+				{"t3a.nano", 2},
+				{"t3a.small", 2},
+				{"t3a.micro", 2},
+				{"t3.small", 2},
+				{"t3.micro", 2},
+				{"t2.xlarge", 4},
+				{"t3.xlarge", 4},
+				{"t3a.xlarge", 4},
+			},
+		},
+		{
+			"get candidates for c5g.xlarge: graviron2 arm 4 vPCU",
+			args{"c6g.xlarge"},
+			[]instanceTypeWeight{
+				{"c6g.xlarge", 4},
+				{"c6gn.xlarge", 4},
+				{"c6gd.xlarge", 4},
+				{"c6g.large", 2},
+				{"c6gd.large", 2},
+				{"c6gn.large", 2},
+				{"c6gn.2xlarge", 8},
+				{"c6g.2xlarge", 8},
+				{"c6gd.2xlarge", 8},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getGoodCandidates(tt.args.instanceType); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("getGoodCandidates() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
